d5f059e2294cfd193a2270ecdbea0054ab05a23c
Atanas Atanasov <72028107+atanasatanasov03@users.noreply.github.com>
2023-10-04 16:01:02 +0300

    Initial Commit

---
+++ RooftopRepairs/Controllers/Contacts.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RooftopRepairs.Controllers
+{
+    public class Contacts : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RooftopRepairs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RooftopRepairs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
